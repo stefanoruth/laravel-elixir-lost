b5002a0ef6bd1d01a6c571bafacc6e5df78819ff
stefanoruth <stefano6262@gmail.com>
2016-07-17 14:58:19 +0200

    Updated Script Version

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
-var Elixir = require('laravel-elixir'),
-	lost = require('lost'),
-	compile = require('laravel-elixir/tasks/shared/Css'),
-	gulp = require('gulp');
-
-var config = Elixir.config;
+var CssTask = require('laravel-elixir/dist/tasks/CssTask').default;
 
 /*
  * Lost Module Config
  */
-config.css.lost = {
-	tempFile: config.publicPath+'/'+config.css.outputFolder+'/lost.css',
+Elixir.config.css.lost = {
+	tempFile: Elixir.config.publicPath+'/'+Elixir.config.css.outputFolder+'/lost.css',
 
+	plugin: require('gulp-postcss'),
 	pluginOptions: [
-		lost()
+		require('lost'),
 	],
 };
 
 /*
  * Lost Task Compiler
  */
-var lostTask = function(src, output) {
+var lostTask = function(output) {
 	var paths = new Elixir.GulpPaths()
-					.src(src, '')
-					.output(output || config.get('public.css.outputFolder'), 'app.css');
+					.src(Elixir.config.css.lost.tempFile, '')
+					.output(output || Elixir.config.get('public.css.outputFolder'), 'app.css');
 
-	new Elixir.Task('lost', function(){
-		return compile({
-			name: 'Lost',
-			compiler: require('gulp-postcss'),
-			src: paths.src,
-			output: paths.output,
-			task: this,
-			pluginOptions: config.css.lost.pluginOptions,
-		});
-	}).watch(paths.src.path);
+	new CssTask('lost', paths).watch(paths.src.path);
 };
 
 /**
  * Lost Compiler for Sass & Scss
  */
 Elixir.extend('sassLost', function() {
-	Elixir.mixins.sass(arguments[0], config.css.lost.tempFile);
-	lostTask(config.css.lost.tempFile, arguments[1]);
+	Elixir.mixins.sass(arguments[0], Elixir.config.css.lost.tempFile);
+	lostTask(arguments[1]);
 });
 
 /**
  * Lost Compiler for Less
  */
 Elixir.extend('lessLost', function() {
-	Elixir.mixins.less(arguments[0], config.css.lost.tempFile);
-	lostTask(config.css.lost.tempFile, arguments[1]);
+	Elixir.mixins.less(arguments[0], Elixir.config.css.lost.tempFile);
+	lostTask(arguments[1]);
 });
 
 /**
  * Lost Compiler for styles
  */
 Elixir.extend('stylesLost', function() {
-	Elixir.mixins.styles(arguments[0], config.css.lost.tempFile);
-	lostTask(config.css.lost.tempFile, arguments[1]);
+	Elixir.mixins.styles(arguments[0], Elixir.config.css.lost.tempFile);
+	lostTask(arguments[1]);
 });
